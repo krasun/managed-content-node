773283b17ec320badb331261d0c3f57e499e740e
Dmitry Krasun <krasun.net@gmail.com>
2016-03-11 00:03:09 +0200

    Rewrite application bootstrap

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -5,6 +5,7 @@ namespace Asopeli\ManagedContentNode;
 use Asopeli\ManagedContentNode\Request\RequestHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Asopeli\ManagedContentNode\RequestHandler;
 
 /**
  * Entry point.
@@ -12,9 +13,89 @@ use Symfony\Component\HttpFoundation\Response;
 class AppKernel
 {
     /**
+     * @var array
+     */
+    private $parameters;
+
+    /**
      * @var RequestHandlerInterface[]
      */
     private $handlers = [];
+
+    /**
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Boots application kernel.
+     */
+    public function boot()
+    {
+        $loader = new \Symfony\Component\Templating\Loader\FilesystemLoader(__DIR__.'/../app/views/%name%');
+        $templating = new \Symfony\Component\Templating\PhpEngine(
+            new \Symfony\Component\Templating\TemplateNameParser(),
+            $loader
+        );
+
+        $templating->addHelpers([
+            new \Symfony\Component\Templating\Helper\SlotsHelper(),
+            new \Knp\Bundle\TimeBundle\Templating\Helper\TimeHelper(
+                new \Knp\Bundle\TimeBundle\DateTimeFormatter(
+                    new \Symfony\Component\Translation\Translator($this->parameters['locale'])
+                )
+            )
+        ]);
+        foreach ($this->parameters['templateVariables'] as $variableName => $variableValue) {
+            $templating->addGlobal($variableName, $variableValue);
+        }
+
+        $connection = \Doctrine\DBAL\DriverManager::getConnection($this->parameters['database'], new \Doctrine\DBAL\Configuration());
+        $pageRepository = new \Asopeli\ManagedContentNode\Entity\Repository\PageRepository($connection);
+        $pageCategoryRepository = new \Asopeli\ManagedContentNode\Entity\Repository\PageCategoryRepository($connection);
+
+        $this
+            ->addHandler(
+                new RequestHandler\GetIndexRequestHandler($pageRepository, $pageCategoryRepository, $templating)
+            )
+            ->addHandler(
+                new RequestHandler\GetSitemapHandler()
+            )
+            ->addHandler(
+                new RequestHandler\PostPageRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\PutPageRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\DeletePageRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\GetPageRequestHandler($pageRepository, $templating)
+            )
+            ->addHandler(
+                new RequestHandler\GetPagesRequestHandler($pageRepository, $pageCategoryRepository, $templating)
+            )
+            ->addHandler(
+                new RequestHandler\PostPageCategoryRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\PutPageCategoryRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\DeletePageCategoryRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\GetPageCategoryRequestHandler()
+            )
+            ->addHandler(
+                new RequestHandler\GetPageCategoriesRequestHandler()
+            )
+        ;
+    }
 
     /**
      * @param RequestHandlerInterface $handler
--- web/app.php
+++ web/app.php
@@ -1,70 +1,11 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+
 $parameters = require_once __DIR__.'/../app/parameters.php';
+$appKernel = new \Asopeli\ManagedContentNode\AppKernel($parameters);
 
-$loader = new \Symfony\Component\Templating\Loader\FilesystemLoader(__DIR__.'/../app/views/%name%');
-$templating = new \Symfony\Component\Templating\PhpEngine(
-    new \Symfony\Component\Templating\TemplateNameParser(),
-    $loader
-);
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$response = $appKernel->handle($request);
 
-$templating->addHelpers([
-    new \Symfony\Component\Templating\Helper\SlotsHelper(),
-    new \Knp\Bundle\TimeBundle\Templating\Helper\TimeHelper(
-        new \Knp\Bundle\TimeBundle\DateTimeFormatter(
-            new \Symfony\Component\Translation\Translator($parameters['locale'])
-        )
-    )
-]);
-foreach ($parameters['templateVariables'] as $variableName => $variableValue) {
-    $templating->addGlobal($variableName, $variableValue);
-}
-
-$connection = \Doctrine\DBAL\DriverManager::getConnection($parameters['database'], new \Doctrine\DBAL\Configuration());
-$pageRepository = new \Asopeli\ManagedContentNode\Entity\Repository\PageRepository($connection);
-$pageCategoryRepository = new \Asopeli\ManagedContentNode\Entity\Repository\PageCategoryRepository($connection);
-
-
-
-$appKernel = (new \Asopeli\ManagedContentNode\AppKernel())
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetIndexRequestHandler($pageRepository, $pageCategoryRepository, $templating)
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetSitemapHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\PostPageRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\PutPageRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\DeletePageRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetPageRequestHandler($pageRepository, $templating)
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetPagesRequestHandler($pageRepository, $pageCategoryRepository, $templating)
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\PostPageCategoryRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\PutPageCategoryRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\DeletePageCategoryRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetPageCategoryRequestHandler()
-    )
-    ->addHandler(
-        new \Asopeli\ManagedContentNode\RequestHandler\GetPageCategoriesRequestHandler()
-    )
-;
-
-$response = $appKernel->handle(\Symfony\Component\HttpFoundation\Request::createFromGlobals());
 $response->send();
