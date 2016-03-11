6f96ea8491e37bb5bd4f78414862d5806c67aa62
Dmitry Krasun <krasun.net@gmail.com>
2016-03-11 23:03:37 +0200

    add twitter bootstrap

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <title><?php if ($view['slots']->has('title')) { echo $view['slots']->output('title'); } ?></title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
     </head>
     <body>
         <header>
@@ -15,5 +18,6 @@
         <footer>
             <?php echo date('Y') ?>
         </footer>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </body>
 </html>
