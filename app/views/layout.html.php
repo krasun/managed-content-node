7d5605a06c000906a8955cea403f7dd5fe7b7ce6
Dmitry Krasun <krasun.net@gmail.com>
2016-03-12 00:13:36 +0200

    Complete design of main page

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -88,7 +88,6 @@
             <?php endif; ?>
             <footer class="footer">
                 <?php echo $view->render('footer.html.php') ?>
-                <p>&copy;<?php echo date('Y') ?> GynecologyNotes</p>
             </footer>
         </div>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
